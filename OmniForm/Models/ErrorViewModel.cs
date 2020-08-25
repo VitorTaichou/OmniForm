e7dbff728a2f06c8e3115305e928fc69cd432d36
Vitor Sobrinho <vitor_vt123@hotmail.com>
2020-08-25 11:31:32 -0400

    Initial Commit
    Signed-off-by: Vitor Sobrinho <vitor_vt123@hotmail.com>

---
+++ OmniForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OmniForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
